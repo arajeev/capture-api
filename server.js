3d5cd7cd5075f3cb76b19704a42ce034e42852b4
arajeev <ranagha13@gmail.com>
2016-11-19 02:05:25 -0500

    access control credentials

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ server.use(
   function crossOrigin(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
     return next();
   }
 );
