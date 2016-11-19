f9ba154687dfd17c542ff56f2ab69c827a2c761b
arajeev <ranagha13@gmail.com>
2016-11-19 02:15:25 -0500

    access control origin set to localhost

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ var server = restify.createServer({
 
 server.use(
   function crossOrigin(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
     res.header("Access-Control-Allow-Credentials", "true");
     return next();
   }
