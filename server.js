61efe8f40fe0dcd3f7f7c30c33d2f5fe4b6cfc66
arajeev <ranagha13@gmail.com>
2016-11-19 02:25:19 -0500

    removed access cred

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ server.use(
   function crossOrigin(req,res,next){
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    res.header("Access-Control-Allow-Credentials", "true");
     return next();
   }
 );
